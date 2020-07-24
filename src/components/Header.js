2ff3b106498373404dae08d3256320997898783c
Nikita Belyakov <nikita.belyakov.38@ya.ru>
2020-07-24 17:50:41 +0300

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className='header bg-primary'>
+            <div className='container'>
+                <h1>Front-End test work</h1>
+                <p>using React and Bootsrap 5</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Main extends React.Component {
+    render() {
+      return (
+        <main>
+          <div className='container'>
+
+          </div>
+        </main>
+      )
+    }
+  }
+  
+  export default Main;
